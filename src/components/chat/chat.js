2bab8e3358406f9047760dedebe101dd00230e54
Semen Titarenko <maktakin@gmail.com>
2021-02-26 17:03:41 +0300

    first version

---
+++ src/components/chat/chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Main = () => {
+    return (
+        <div>
+            тут окно чатов
+        </div>
+    )
+}
+export default Main
